a5ee0a1fc6d00fd2a4e584da31b91edf6cdb36e3
LearnWithRehan <learnwithrehan86147@gmail.com>
2026-03-28 23:26:06 +0530

    add pattern 26,27 and 28

---
+++ Pattern26.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Pattern26 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter num of rows:- ");
+        int rows = sc.nextInt();
+        for (int i = 1; i<=rows; i++){
+            for (int j = 1; j<=i; j++){
+                if (j%2==0){
+                    System.out.print(0);
+                }else {
+                    System.out.print(1);
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
---
+++ Pattern27.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Pattern27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter num of rows:- ");
+        int rows = sc.nextInt();
+        for (int i = 1; i<=rows; i++){
+            int num;
+            if (i % 2 == 0){
+                num = 0;
+                for (int j = 1; j<=rows; j++){
+                    System.out.print(num);
+                    num = (num == 0) ? 1 : 0;
+                }
+            }else {
+                num = 1;
+                for (int j = 1; j<=rows; j++){
+                    System.out.print(num);
+                    num = (num == 0) ? 1 : 0;
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
