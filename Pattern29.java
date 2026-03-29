594c01660dc229d727e6556d2d0df304596e0339
LearnWithRehan <learnwithrehan86147@gmail.com>
2026-03-29 23:51:17 +0530

    add pattern 29,30,31 and 32

---
+++ Pattern29.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Pattern29 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter num of rows");
+        int rows = sc.nextInt();
+        for (int i = 0; i<rows; i++){
+            for (int j = 0; j<rows; j++){
+                if (i == j){
+                    System.out.print(i);
+                }else {
+                    System.out.print(0);
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
