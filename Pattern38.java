fd0c10028e8180960c5ba81bcbe48740efd7db1a
LearnWithRehan <learnwithrehan86147@gmail.com>
2026-04-02 17:12:56 +0530

    Add pattern 36,37 and 38

---
+++ Pattern38.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Pattern38 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter num of rows");
+        int rows =sc.nextInt();
+        for (int i = 1; i<=rows; i++){
+            int num = 1;
+            for (int j = 1; j<i*2; j++){
+                if (j % 2 == 0){
+                    System.out.print("*");
+                }else {
+                    System.out.print(num);
+                    num++;
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
