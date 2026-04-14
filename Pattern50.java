aacf6cf53bfc55d1a19fd8249dc2d675c6cfb7ac
LearnWithRehan <learnwithrehan86147@gmail.com>
2026-04-15 00:00:32 +0530

    add pattern 49 and 50

--- Pattern50.java
+++ Pattern50.java
@@ -1,31 +1,32 @@
 import java.util.Scanner;
 
-public class Pattern49 {
+public class Pattern50 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter num of rows:- ");
         int rows = sc.nextInt();
         for (int i = 1; i<=rows; i++){
-            //for space
-            for (int j = 1; j<=rows-i; j++){
+            //for spaces
+            for (int j = 2*rows-i; j>i; j--){
                 System.out.print(" ");
             }
-            for (int j = 1; j<=i; j++){
+            for (int j = i; j>=1; j--){
                 System.out.print("* ");
             }
             System.out.println();
         }
 
-        for (int i = rows-1; i>=1; i--){
+        for (int i= rows-1; i>=1; i--){
             //for spaces
-            for (int j = 1; j<=rows-i; j++){
+            for (int j = 2*rows-i; j>i; j--){
                 System.out.print(" ");
             }
-            for (int j = 1; j<=i; j++){
+            for (int j = i; j>=1; j--){
                 System.out.print("* ");
             }
             System.out.println();
         }
+
 
         sc.close();
     }
