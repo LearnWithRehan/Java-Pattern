1441d8a23f414376ceae8f19b67df134870f5725
LearnWithRehan <learnwithrehan86147@gmail.com>
2026-04-21 23:30:57 +0530

    Add Pattern 54 and 55

---
+++ Pattern55.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Pattern55 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter num of rows:- ");
+        int rows = sc.nextInt();
+        for (int i = rows; i>=1; i--){
+            for (int j = 1; j<=i; j++){
+                //for spaces
+                if (j == 1 || i == j || i == rows){
+                    System.out.print("* ");
+                }else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
