36126130ff7a95a430c74e581a83f48d98698763
LearnWithRehan <learnwithrehan86147@gmail.com>
2026-05-03 08:54:21 +0530

    add Pattern 70 to 72

--- Pattern71.java
+++ Pattern71.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 
-public class Pattern70 {
+public class Pattern71 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter num of rows:- ");
         int rows = sc.nextInt();
         char ch;
         for (int i = rows; i>=1; i--){
-            ch = 'G';
+            ch = 'P';
             for (int j = 1; j<=i; j++){
                 System.out.print(ch + " ");
-                ch--;
+                ch++;
             }
             System.out.println();
+
         }
         sc.close();
     }
---
+++ Pattern72.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Pattern72{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter num of rows:-");
+        int rows = sc.nextInt();
+        char ch;
+        for (int i = 1; i<=rows; i++){
+            ch = 'A';
+            //spcaces
+            for (int j = 1; j<=rows-i; j++){
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j<=i; j++){
+                System.out.print(ch+ " ");
+                ch++;
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
